9af60a964f6dcff5948f1d54e3d49eb442ce7c8f
olo <6811850007@mut.ac.th>
2026-01-06 20:52:49 +0700

    7

---
+++ lab07/lab7_4/lab7_4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = { 10, 20, 30, 40 };
+    int* p = arr;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << *(p + i) << " ";
+    }
+    cout << endl;
+    return 0;
+}
